22b540eaf0c9dcf528f547292bd90b5c31f7e062
dakavin <mikeylay@126.com>
2024-04-02 19:04:58 +0800

    Site updated: 2024-04-02 19:04:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/fac34f51.html","posts/83f127d5.html","posts/56705aa1.html","posts/d68e5e2f.html","posts/4110bbe2.html","posts/4110bbe2.html","posts/d4b6632d.html","posts/678d5250.html","posts/6c17d6bd.html","posts/c636a720.html"];function toRandomPost(){
+var posts=["posts/fac34f51.html","posts/83f127d5.html","posts/d68e5e2f.html","posts/4110bbe2.html","posts/56705aa1.html","posts/d4b6632d.html","posts/4110bbe2.html","posts/6c17d6bd.html","posts/678d5250.html","posts/c636a720.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
