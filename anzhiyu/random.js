63767dc6017d3296428e0bed92875ab86cfa4121
dakavin <mikeylay@126.com>
2024-04-05 15:45:16 +0800

    Site updated: 2024-04-05 15:45:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9be2ea07.html","posts/fac34f51.html","posts/83f127d5.html","posts/b7e144d1.html","posts/56705aa1.html","posts/4110bbe2.html","posts/d4b6632d.html","posts/6c17d6bd.html","posts/4110bbe2.html","posts/678d5250.html","posts/c636a720.html"];function toRandomPost(){
+var posts=["posts/fac34f51.html","posts/83f127d5.html","posts/4110bbe2.html","posts/56705aa1.html","posts/b7e144d1.html","posts/4110bbe2.html","posts/d4b6632d.html","posts/9be2ea07.html","posts/6c17d6bd.html","posts/678d5250.html","posts/c636a720.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
